16c681f57434e82075d722ab432a19d3724730cd
Kavintha Kulasingham <kmuthisha@gmail.com>
2026-03-05 14:22:41 +0530

    feat: initial project setup with Tauri, React, and Vite

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+	// Tauri expects a fixed port
+	clearScreen: false,
+	server: {
+		port: 1420,
+		strictPort: true,
+		watch: {
+			ignored: ["**/src-tauri/**"],
+		},
+	},
+	// to make use of `TAURI_DEBUG` and other env variables
+	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+	envPrefix: ["VITE_", "TAURI_"],
+	build: {
+		// Tauri supports es2021
+		target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
+		// don't minify for debug builds
+		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+		// produce sourcemaps for debug builds
+		sourcemap: !!process.env.TAURI_DEBUG,
+	},
+	base: "./",
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
